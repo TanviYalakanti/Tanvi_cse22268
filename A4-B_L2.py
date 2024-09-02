4179d0ee4c4a707b379d3fafd826c86e453ba3bb
TanviYalakanti <yelakantitanvi@gmail.com>
2024-09-02 14:43:35 +0530

    Update and rename A4-B.py to A4-B_L2.py

--- A4-B_L2.py
+++ A4-B_L2.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# <a href="https://colab.research.google.com/github/deva-kumari/devakumari_cse22237/blob/main/A4-B.ipynb" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a>
-
-# In[19]:
-
-
 # Convert 'Date' column to datetime
 df_stock['Date'] = pd.to_datetime(df_stock['Date'])
 
@@ -19,4 +11,3 @@ mean_wednesday_price = statistics.mean(wednesday_prices)
 # Compare with population mean
 print(f"Mean Price on Wednesdays: {mean_wednesday_price}")
 print(f"Population Mean Price: {mean_price}")
-
